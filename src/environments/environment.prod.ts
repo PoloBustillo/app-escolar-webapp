a48744190a3ae80469e79d418ca5c2572bcd522d
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2025-11-25 19:56:28 -0600

    fix /

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_api: 'https://appescolarapi-production-b5ec.up.railway.app/',
+  url_api: 'https://appescolarapi-production-b5ec.up.railway.app',
 };
